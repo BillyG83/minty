887810b56e008fa95d49604d68322645c5614b35
billgil <bill@moralis.io>
2024-03-22 11:09:04 +0000

    fix: app wont fall over if user has no wallet

--- src/web3.ts
+++ src/web3.ts
@@ -1,7 +1,7 @@
 import Web3 from "web3";
  
 // Use type assertion for ethereum
-(window as any).ethereum.request({ method: "eth_requestAccounts" });
+(window as any)?.ethereum?.request({ method: "eth_requestAccounts" });
 
 // Use type assertion for web3
 const web3 = new Web3((window as any).ethereum);
